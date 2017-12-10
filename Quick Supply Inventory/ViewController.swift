e8112a78651435cc3c44db049922025f3c152a78
user907787 <user907787@ny211.local>
2017-12-09 21:14:25 -0500

    Initial Commit

---
+++ Quick Supply Inventory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quick Supply Inventory
+//
+//  Created by user907787 on 12/9/17.
+//  Copyright © 2017 Andrew Hopps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
